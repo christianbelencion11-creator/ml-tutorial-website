a50151a0e2ac268650c32afd122131dc1de78d2f
christianbelencion11-creator <christianbelencion11@gmail.com>
2026-03-01 13:54:34 +0800

    fix logo navigation

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { Container, Navbar, Nav } from 'react-bootstrap'
 import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom'
 import { useState } from 'react'
-import { FaGamepad, FaSkull, FaFire, FaCrown, FaUserShield } from 'react-icons/fa'
+import { FaGamepad, FaFire, FaCrown, FaUserShield, FaCommentDots } from 'react-icons/fa'
 import Home from './components/Home'
 import Tutorials from './components/Tutorials'
 import About from './components/About'
@@ -11,23 +11,32 @@ import Contact from './components/Contact'
 import Admin from './components/Admin'
 import TutorialDetail from './components/TutorialDetail'
 
-// ✅ Separate inner component so we can use useNavigate inside Router
 function AppContent() {
   const [searchTerm, setSearchTerm] = useState('')
+  const [navExpanded, setNavExpanded] = useState(false)
   const navigate = useNavigate()
 
   const handleSearchKeyDown = (e) => {
     if (e.key === 'Enter' && searchTerm.trim()) {
       navigate('/tutorials')
+      setNavExpanded(false)
     }
   }
 
+  const handleNavClick = () => setNavExpanded(false)
+
   return (
     <div className="App">
-      {/* Navigation Bar */}
-      <Navbar className="pmc-navbar" expand="lg" fixed="top" variant="dark">
+      <Navbar
+        className="pmc-navbar"
+        expand="lg"
+        fixed="top"
+        variant="dark"
+        expanded={navExpanded}
+        onToggle={(expanded) => setNavExpanded(expanded)}
+      >
         <Container>
-          <Navbar.Brand as={Link} to="/" className="pmc-brand">
+          <Navbar.Brand as={Link} to="/" className="pmc-brand" onClick={handleNavClick}>
             <img
               src="https://scontent.fmnl30-3.fna.fbcdn.net/v/t39.30808-6/322707149_1841631216235507_1073256195438098560_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=1d70fc&_nc_ohc=jFDwVIaZtScQ7kNvwEg0lvg&_nc_oc=Adk2WMixZ9b4un2aRcg1Bo7K3kaMNJmIMV4wqchF5p9rIqekrgK3isNqlqtt2O49QrE&_nc_zt=23&_nc_ht=scontent.fmnl30-3.fna&_nc_gid=27SX6zTygGpOBStnP2yJgA&oh=00_AftJl8UEOFcZHUI1yIMRP477cjCcJJ4nk6nKnaMx_a5n4A&oe=699B876C"
               alt="PMC Gaming Logo"
@@ -40,23 +49,23 @@ function AppContent() {
           </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="pmc-nav" />
+
           <Navbar.Collapse id="pmc-nav">
             <Nav className="mx-auto pmc-nav-links">
-              <Nav.Link as={Link} to="/" className="nav-item">
+              <Nav.Link as={Link} to="/" className="nav-item" onClick={handleNavClick}>
                 <FaFire className="nav-icon" /> HOME
               </Nav.Link>
-              <Nav.Link as={Link} to="/tutorials" className="nav-item">
+              <Nav.Link as={Link} to="/tutorials" className="nav-item" onClick={handleNavClick}>
                 <FaGamepad className="nav-icon" /> TUTORIALS
               </Nav.Link>
-              <Nav.Link as={Link} to="/about" className="nav-item">
+              <Nav.Link as={Link} to="/about" className="nav-item" onClick={handleNavClick}>
                 <FaCrown className="nav-icon" /> ABOUT
               </Nav.Link>
-              <Nav.Link as={Link} to="/contact" className="nav-item">
-                <FaSkull className="nav-icon" /> CONTACT
+              <Nav.Link as={Link} to="/contact" className="nav-item" onClick={handleNavClick}>
+                <FaCommentDots className="nav-icon" /> CONTACT
               </Nav.Link>
             </Nav>
 
-            {/* Search Box */}
             <div className="nav-right">
               <div className="pmc-search">
                 <input
@@ -68,7 +77,7 @@ function AppContent() {
                   className="pmc-search-input"
                 />
               </div>
-              <Nav.Link as={Link} to="/admin" className="pmc-admin-btn">
+              <Nav.Link as={Link} to="/admin" className="pmc-admin-btn" onClick={handleNavClick}>
                 <FaUserShield className="admin-btn-icon" />
                 <span className="admin-btn-text">ADMIN</span>
               </Nav.Link>
@@ -77,7 +86,6 @@ function AppContent() {
         </Container>
       </Navbar>
 
-      {/* Main Content */}
       <Container className="mt-5 pt-5 main-content">
         <Routes>
           <Route path="/" element={<Home searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
